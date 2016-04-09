ad358c4df05291136d05c5d368896829e2294e45
Yannis Gonianakis <jgonian@gmail.com>
2016-04-09 22:10:45 +0200

    Upgrade scalatest to v2.2.6.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val commonsIpMath     =   "com.github.jgonian"  %   "commons-ip-math" % "1.30"
-  val scalaTest         =   "org.scalatest"       %%  "scalatest"       % "2.2.4"   % Test
+  val scalaTest         =   "org.scalatest"       %%  "scalatest"       % "2.2.6"   % Test
 
   val mainDependencies = Seq(commonsIpMath)
 
