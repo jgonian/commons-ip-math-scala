ac4ce79e91788a7d7e1395a5ddfa2bcac93f80df
Yannis Gonianakis <jgonian@gmail.com>
2016-04-09 18:44:52 +0200

    Depend on lastest commons-ip-math (v1.30)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
 
-  val commonsIpMath     =   "net.ripe.commons"  %   "commons-ip-math" % "1.22"
-  val scalaTest         =   "org.scalatest"     %%  "scalatest"       % "2.2.4"   % Test
+  val commonsIpMath     =   "com.github.jgonian"  %   "commons-ip-math" % "1.30"
+  val scalaTest         =   "org.scalatest"       %%  "scalatest"       % "2.2.4"   % Test
 
   val mainDependencies = Seq(commonsIpMath)
 
